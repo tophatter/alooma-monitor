5f1d29f284b1d1f90ab99900c31dcb966b03fa50
Lindsay Anchors <lindsaya@tophatter.comlindsaya@tophatter.com>
2019-01-12 23:35:53 -0800

    Remove timeout arg causing error

--- api_config.py
+++ api_config.py
@@ -4,11 +4,9 @@ import alooma
 ALOOMA_USERNAME = os.environ.get('ALOOMA_USERNAME')
 ALOOMA_PASSWORD = os.environ.get('ALOOMA_PASSWORD')
 ALOOMA_ACCOUNT_NAME = os.environ.get('ALOOMA_ACCOUNT_NAME')
-REQUEST_TIMEOUT = os.environ.get('REQUEST_TIMEOUT', 300)
 
 api = alooma.Client(
     username=ALOOMA_USERNAME,
     password=ALOOMA_PASSWORD,
-    account_name=ALOOMA_ACCOUNT_NAME,
-    timeout=REQUEST_TIMEOUT
+    account_name=ALOOMA_ACCOUNT_NAME
 )
