b9ca3af9aacc90cf65ca2e9a65a16410238b4f67
Lindsay Anchors <lindsaya@tophatter.comlindsaya@tophatter.com>
2019-01-12 22:04:01 -0800

    Use limited in-range instead of while TRUE

--- restream_job.py
+++ restream_job.py
@@ -5,19 +5,16 @@ from api_config import api
 MAX_RETRIES = os.environ.get('RESTREAM_MAX_RETRIES', 10)
 
 if __name__ == '__main__':
-  print "Restreaming alooma queue."
-  retries = 0
-
-  while True:
-      try:
-          api.start_restream()
-          print "Successfully restreamed alooma queue."
-      except requests.exceptions.Timeout as e:
-          print e
-          if retries <= MAX_RETRIES
-              "Retrying..."
-              retries += 1
-              continue
-      except Exception as e:
-          print e
-      break
+    print "Restreaming alooma queue."
+    for _ in range(MAX_RETRIES)::
+        try:
+            api.start_restream()
+            print "Successfully restreamed alooma queue."
+        except requests.exceptions.Timeout as e:
+            print e
+            print "Retrying..."
+            continue
+        except Exception as e:
+            print e
+        else:
+            break
