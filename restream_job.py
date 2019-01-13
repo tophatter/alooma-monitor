ec75739025ac4c8bf04bdd432bdd10a4b70a97b9
Lindsay Anchors <lindsaya@tophatter.comlindsaya@tophatter.com>
2019-01-12 21:46:58 -0800

    Reduce timeout and retry, make retries and timeout envvars

--- restream_job.py
+++ restream_job.py
@@ -1,9 +1,23 @@
+from os import environ
+from requests import exceptions
 from api_config import api
 
+MAX_RETRIES = os.environ.get('RESTREAM_MAX_RETRIES', 10)
+
 if __name__ == '__main__':
-    print "Restreaming alooma queue."
-    try:
-      api.start_restream()
-      print "Successfully restreamed alooma queue."
-    except Exception as e:
-      print e
+  print "Restreaming alooma queue."
+  retries = 0
+
+  while True:
+      try:
+          api.start_restream()
+          print "Successfully restreamed alooma queue."
+      except requests.exceptions.Timeout as e:
+          print e
+          if retries <= MAX_RETRIES
+              "Retrying..."
+              retries += 1
+              continue
+      except Exception as e:
+          print e
+      break
