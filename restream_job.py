7f834d0ed6363df427238e062e17b2b04737e39f
Lindsay Anchors <lindsaya@tophatter.comlindsaya@tophatter.com>
2019-01-12 23:42:10 -0800

    Stupid typo

--- restream_job.py
+++ restream_job.py
@@ -7,7 +7,7 @@ MAX_RETRIES = os.environ.get('RESTREAM_MAX_RETRIES', 3)
 
 if __name__ == '__main__':
     print "Restreaming alooma queue."
-    for _ in range(MAX_RETRIES)::
+    for _ in range(MAX_RETRIES):
         try:
             api.start_restream()
             print "Successfully restreamed alooma queue."
