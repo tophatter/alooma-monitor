167909829bcec14a34c7a18cf98080f065cf5bb6
Lindsay Anchors <lindsaya@tophatter.comlindsaya@tophatter.com>
2019-01-12 22:26:07 -0800

    Sleep between retries

--- restream_job.py
+++ restream_job.py
@@ -1,5 +1,6 @@
 from os import environ
 from requests import exceptions
+from time import sleep
 from api_config import api
 
 MAX_RETRIES = os.environ.get('RESTREAM_MAX_RETRIES', 3)
@@ -13,6 +14,7 @@ if __name__ == '__main__':
         except requests.exceptions.Timeout as e:
             print e
             print "Retrying..."
+            sleep(5)
             continue
         except Exception as e:
             print e
